d0706a4bb0625ee0f52d74f41ed217aa903b4bef
BazireJeremy <jeremybazire83@gmail.com>
2022-05-06 17:08:49 +0200

    fix database error

--- packages/devmilleniumprod/contact/src/ContactServiceProvider.php
+++ packages/devmilleniumprod/contact/src/ContactServiceProvider.php
@@ -29,9 +29,9 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/migrations');
         $this->loadViewsFrom(__DIR__.'/views', 'contact');
 
-        $this->publishes([
+        /*$this->publishes([
             __DIR__.'../../../database/migrations/' => database_path('migrations'),
             __DIR__.'../../../routes/web.php' => __DIR__.'/routes.php'
-        ], 'contact-default');
+        ], 'contact-default');*/
     }
 }
